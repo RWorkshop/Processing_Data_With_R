248c0aac518cb5c4ffa8a1f8004b5a3cea368c24
DragonflyStats <kobriendublin@gmail.com>
2023-09-16 22:55:01 +0100

    reload

---
+++ R_packages/inspectdf/inspectdf.R
@@ -0,0 +1,26 @@
+Package 'inspectdf'
+June 27, 2019
+Title Inspection, Comparison and Visualisation of Data Frames
+Version 0.0.3
+Maintainer Alastair Rushworth <alastairmrushworth@gmail.com>
+  Description A collection of utilities for columnwise summary, comparison and visualisation
+of data frames. Functions report missingness, categorical levels, numeric distribution, correlation,
+column types and memory usage.
+* Language en_GB
+* LinkingTo Rcpp
+* LazyLoad yes
+* ByteCompile yes
+* Encoding UTF-8
+* Depends R (>= 3.1.0)
+* Imports dplyr, ggplot2, magrittr, progress, Rcpp, tibble, tidyr,
+* ggfittext (>= 0.8.0)
+* Suggests testthat
+* License GPL-2
+* URL http://github.com/alastairrushworth/inspectdf
+BugReports http://github.com/alastairrushworth/inspectdf/issues
+RoxygenNote 6.1.1
+NeedsCompilation yes
+Author Alastair Rushworth [aut, cre],
+David Wilkins [ctb]
+Repository CRAN
+Date/Publication 2019-06-27 09:20:03 UTC
